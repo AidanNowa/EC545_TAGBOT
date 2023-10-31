adf1107e794b5a3d9d632c8aa1b2a251c7e586db
tomqiluo <tomqiluo@bu.edu>
2023-11-01 03:35:41 +0800

    Initialization with catkin_make

---
+++ tagbot_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jetson/Desktop/EC545_TAGBOT/tagbot_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/jetson/Desktop/EC545_TAGBOT/tagbot_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jetson/Desktop/EC545_TAGBOT/tagbot_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH='/home/jetson/Desktop/EC545_TAGBOT/tagbot_ws/src:/home/jetson/software/library_ws/src:/home/jetson/yahboomcar_ws/src:/home/jetson/software/world_canvas/src:/opt/ros/melodic/share'

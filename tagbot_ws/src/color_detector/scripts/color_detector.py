40ef68801f60133cb519cc62c27f7c2f2f885c24
Shuhei Fujita <shuheifujita95@gmail.com>
2023-12-05 18:27:22 -0500

    Create a custom msg for target position

--- tagbot_ws/src/color_detector/scripts/color_detector.py
+++ tagbot_ws/src/color_detector/scripts/color_detector.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # coding:utf-8
+import time
 import rospy
 import cv2
 import numpy as np
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
-from opencv_apps.msg import Rect, RectArray
+from color_detector.msg import TargetPosition
 
 CONTOUR_AREA_THRESHOLD = 300
 DEPTH_ENCODING = '32FC1'
@@ -21,13 +22,13 @@ class ColorDetector:
         self.depth_image = None
         self.sub_rgb = rospy.Subscriber('/camera/rgb/image_raw', Image, self.register_rgb_image, queue_size=1)
         self.sub_depth = rospy.Subscriber('/camera/depth/image_raw', Image, self.register_depth_image, queue_size=1)
-        # self.pub_position = rospy.Publisher('target_position', tuple, queue_size=10)
+        self.pub_position = rospy.Publisher('target_position', TargetPosition, queue_size=10)
 
     def on_shutdown(self):
         cv2.destroyAllWindows()
         self.sub_rgb.unregister()
         self.sub_depth.unregister()
-        # self.pub_position.unregister()
+        self.pub_position.unregister()
 
     def register_rgb_image(self, message):
         if not isinstance(message, Image): return
@@ -55,7 +56,7 @@ class ColorDetector:
             area = cv2.contourArea(contour)
             if area < CONTOUR_AREA_THRESHOLD: continue
             x, y, w, h = cv2.boundingRect(contour)
-            rect = Rect(x, y, w, h)
+            rect = tuple(x, y, w, h)
             rect_list.append(rect)
             image_frame = cv2.rectangle(image_frame, (x, y), (x + w, y + h), (255, 0, 0), 2) 
         # print('red contours detected :', len(rect_list))
@@ -75,15 +76,18 @@ class ColorDetector:
             if distance < closest_distance:
                 closest_target = [x_center, y_center]
                 closest_distance = distance
+        if closest_target is None: return None
         x_center = closest_target[0]
-        target_angle = x_center / IMAGE_WIDTH * FOV_H - FOV_H / 2
-        target_position = (target_angle, closest_distance)
+        target_position = TargetPosition()
+        target_position.timestamp = time.time()
+        target_position.distance = closest_distance
+        target_position.angle = x_center / IMAGE_WIDTH * FOV_H - FOV_H / 2
         return target_position
 
     def publish_target_position(self, target_position):
         if target_position is None: return
-        print('Publish target_position ', target_position)
-        # self.pub_position.publish(target_position)
+        print('Publish target_position: {}, {}, {}', target_position.timestamp, target_position.distance, target_position.angle)
+        self.pub_position.publish(target_position)
 
 
 if __name__ == '__main__':

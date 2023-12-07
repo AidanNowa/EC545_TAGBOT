bd22114b991aa8b14a0699cdf6a7bec1e3ce452f
Shuhei Fujita <shuheifujita95@gmail.com>
2023-12-07 13:51:37 -0500

    Add color indicator

--- tagbot_ws/src/controller/scripts/controller.py
+++ tagbot_ws/src/controller/scripts/controller.py
@@ -9,8 +9,10 @@ from sensor_msgs.msg import LaserScan
 from dynamic_reconfigure.server import Server
 from yahboomcar_laser.cfg import laserAvoidPIDConfig
 from Rosmaster_Lib import Rosmaster
+
 RAD2DEG = 180 / math.pi
 TAG_THRESHOLD = 0.6 # meters
+TURN_ANGLE = 0.5 # radian
 
 
 class TagBotController:
@@ -18,7 +20,6 @@ class TagBotController:
         rospy.on_shutdown(self.cancel)
         self.bot = Rosmaster()
         self.r = rospy.Rate(20)
-        self.Moving = False
         self.switch = False
         self.right_warning = False
         self.left_warning = False
@@ -27,9 +28,6 @@ class TagBotController:
         self.lin_pid = SinglePID(2.0, 0.0, 2.0)
         self.ang_pid = SinglePID(3.0, 0.0, 5.0)
         Server(laserAvoidPIDConfig, self.dynamic_reconfigure_callback)
-        self.linear = 0.5
-        self.angular = 1.0
-        self.prev_linear = 0
         self.latest_position = None
         self.sub_laser = rospy.Subscriber('/scan', LaserScan, self.register_scan, queue_size=1)
         self.sub_position = rospy.Subscriber('/tagbot/target_position', Vector3Stamped, self.register_position, queue_size = 1)
@@ -65,20 +63,18 @@ class TagBotController:
         self.update_obstacle_warnings(scan_data)
         if self.is_obstacles_detected():
             # Turn away from the obstacles
+            self.set_color_light('yellow')
             self.avoid_obstacles()
         else:
             if self.is_target_detected():
                 # TODO If tagged
                 # Chase the target
+                self.set_color_light('red')
                 self.chase_target()
             else:
                 # Search the area
-                pass
-        # if self.ros_ctrl.Joy_active or self.switch == True:# or self.active == False:
-        #     if self.Moving == True:
-        #         self.ros_ctrl.pub_vel.publish(Twist())
-        #         self.Moving = not self.Moving
-        #         return
+                self.set_color_light('blue')
+                self.move_robot(distance=0, angle=20)
         self.r.sleep()
 
     def update_obstacle_warnings(self, scan_data):
@@ -104,7 +100,7 @@ class TagBotController:
     
     def move_robot(self, distance, angle):
         twist = Twist()
-        twist.linear.x = self.prev_linear = distance
+        twist.linear.x = distance
         twist.angular.z = angle
         self.ros_ctrl.pub_vel.publish(twist)
         sleep(0.2)
@@ -112,26 +108,26 @@ class TagBotController:
     def avoid_obstacles(self):
         if self.front_warning and self.left_warning and self.right_warning:
             # Back up and turn right
-            self.move_robot(distance=-0.15, angle=-self.angular)
+            self.move_robot(distance=-0.15, angle=-TURN_ANGLE)
         elif self.front_warning and not self.left_warning and self.right_warning:
             # Turn left
-            self.move_robot(distance=0, angle=self.angular)
+            self.move_robot(distance=0, angle=TURN_ANGLE)
         elif self.front_warning and self.left_warning and not self.right_warning:
             # Turn right
-            self.move_robot(distance=0, angle=-self.angular)
+            self.move_robot(distance=0, angle=-TURN_ANGLE)
         elif self.front_warning and not self.left_warning and not self.right_warning:
             # Turn left
-            self.move_robot(distance=0, angle=self.angular)
+            self.move_robot(distance=0, angle=TURN_ANGLE)
         elif not self.front_warning and self.left_warning and self.right_warning:
             # Turn right
-            self.move_robot(distance=0, angle=-self.angular)
+            self.move_robot(distance=0, angle=-TURN_ANGLE)
             sleep(0.2)
         elif not self.front_warning and self.left_warning and not self.right_warning:
             # Turn right
-            self.move_robot(distance=0, angle=-self.angular)
+            self.move_robot(distance=0, angle=-TURN_ANGLE)
         elif not self.front_warning and not self.left_warning and self.right_warning:
             # Turn left
-            self.move_robot(distance=0, angle=self.angular)
+            self.move_robot(distance=0, angle=TURN_ANGLE)
 
     def chase_target(self):
         target_distance = self.latest_position.vector.x / 1000.0
@@ -141,6 +137,16 @@ class TagBotController:
         if abs(angle) < 0.02:
             angle = 0
         self.move_robot(distance=distance, angle=angle)
+
+    def set_color_light(self, color):
+        if color == 'red':
+            self.bot.set_colorful_lamps(0xff, 255, 0, 0)
+        elif color == 'yellow':
+            self.bot.set_colorful_lamps(0xff, 255, 255, 0)
+        elif color == 'blue':
+            self.bot.set_colorful_lamps(0xff, 0, 0, 255)
+        elif color == 'green':
+            self.bot.set_colorful_lamps(0xff, 0, 255, 0)
 
 
 if __name__ == '__main__':
